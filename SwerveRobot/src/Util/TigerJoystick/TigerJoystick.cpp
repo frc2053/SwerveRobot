e5bcb7e0b1f3cf1e3e00a7de94cb961a7deb149c
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2017-08-14 11:46:48 -0400

    Update TigerJoystick.cpp

--- SwerveRobot/src/Util/TigerJoystick/TigerJoystick.cpp
+++ SwerveRobot/src/Util/TigerJoystick/TigerJoystick.cpp
@@ -1,62 +1,62 @@
 #include "TigerJoystick.h"
 
 TigerJoystick::TigerJoystick(int port) {
-	joystick.reset(new frc::Joystick(port));
-	aButton.reset(new frc::JoystickButton(joystick.get(), 1));
-	bButton.reset(new frc::JoystickButton(joystick.get(), 2));
-	xButton.reset(new frc::JoystickButton(joystick.get(), 3));
-	yButton.reset(new frc::JoystickButton(joystick.get(), 4));
-	leftShoulderButton.reset(new frc::JoystickButton(joystick.get(), 5));
-	rightShoulderButton.reset(new frc::JoystickButton(joystick.get(), 6));
-	selectButton.reset(new frc::JoystickButton(joystick.get(), 7));
-	startButton.reset(new frc::JoystickButton(joystick.get(), 8));
-	leftStickButton.reset(new frc::JoystickButton(joystick.get(), 9));
-	rightStickButton.reset(new frc::JoystickButton(joystick.get(), 10));
-	leftTrigger.reset(new TigerLeftTrigger(joystick.get(), 3));
-	rightTrigger.reset(new TigerRightTrigger(joystick.get(), 3));
+	joystick = frc::Joystick(port);
+	aButton = frc::JoystickButton(joystick, 1);
+	bButton = frc::JoystickButton(joystick, 2);
+	xButton = frc::JoystickButton(joystick, 3);
+	yButton = frc::JoystickButton(joystick, 4);
+	leftShoulderButton = frc::JoystickButton(joystick, 5);
+	rightShoulderButton = frc::JoystickButton(joystick, 6);
+	selectButton = frc::JoystickButton(joystick, 7);
+	startButton = frc::JoystickButton(joystick, 8);
+	leftStickButton = frc::JoystickButton(joystick, 9);
+	rightStickButton = frc::JoystickButton(joystick, 10);
+	leftTrigger = TigerLeftTrigger(joystick, 3);
+	rightTrigger = TigerRightTrigger(joystick, 3);
 }
 
 TigerJoystick::~TigerJoystick() {
 }
 
 double TigerJoystick::GetLeftXAxis() {
-	return DeadBandJoystick(joystick->GetRawAxis(0));
+	return DeadBandJoystick(joystick.GetRawAxis(0));
 }
 
 double TigerJoystick::GetLeftYAxis() {
-	return DeadBandJoystick(joystick->GetRawAxis(1));
+	return DeadBandJoystick(joystick.GetRawAxis(1));
 }
 
 double TigerJoystick::GetRightXAxis() {
-	return DeadBandJoystick(joystick->GetRawAxis(4));
+	return DeadBandJoystick(joystick.GetRawAxis(4));
 }
 
 double TigerJoystick::GetRightYAxis() {
-	return DeadBandJoystick(joystick->GetRawAxis(5));
+	return DeadBandJoystick(joystick.GetRawAxis(5));
 }
 
 double TigerJoystick::GetLeftTriggerValue() {
-	return leftTrigger->GetTriggerValue();
+	return leftTrigger.GetTriggerValue();
 }
 
 double TigerJoystick::GetRightTriggerValue() {
-	return rightTrigger->GetTriggerValue();
+	return rightTrigger.GetTriggerValue();
 }
 
 bool TigerJoystick::GetLeftTriggerPressed() {
-	return leftTrigger->Get();
+	return leftTrigger.Get();
 }
 
 bool TigerJoystick::GetRightTriggerPressed() {
-	return rightTrigger->Get();
+	return rightTrigger.Get();
 }
 
-TigerLeftTrigger* TigerJoystick::GetLeftTrigger() {
-	return leftTrigger.get();
+TigerLeftTrigger TigerJoystick::GetLeftTrigger() {
+	return leftTrigger;
 }
 
-TigerRightTrigger* TigerJoystick::GetRightTrigger() {
-	return rightTrigger.get();
+TigerRightTrigger TigerJoystick::GetRightTrigger() {
+	return rightTrigger;
 }
 
 double TigerJoystick::DeadBandJoystick(double axis) {
