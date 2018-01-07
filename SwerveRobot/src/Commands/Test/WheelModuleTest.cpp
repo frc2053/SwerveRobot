d3b46319497d2d41ec0decda9ff0a29cb4891a37
Tigertronics <team2053tigertronics@gmail.com>
2018-01-07 13:57:43 -0500

    working on new code

--- SwerveRobot/src/Commands/Test/WheelModuleTest.cpp
+++ SwerveRobot/src/Commands/Test/WheelModuleTest.cpp
@@ -12,21 +12,16 @@ void WheelModuleTest::Initialize() {
 
 void WheelModuleTest::Execute() {
 	//test rotations
-	for(int i = 0; i < 365; i = i + 45) {
+	/*for(int i = 0; i < 365; i = i + 45) {
 		for(int j = 0; j < 4; j++) {
-			Robot::swerveSubsystem->GetSwerveStuff()->GetModules()->at(j).Set(0, Rotation2D::fromDegrees(i));
 		}
+		//Robot::swerveSubsystem->GetSwerveStuff()->GetModules()->at(2).Set(0, Rotation2D::fromDegrees(i));
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	}
-	/*RobotMap::swerveSubsystemBackLeftRotationTalon->SetControlMode(CANTalon::kPercentVbus);
-	RobotMap::swerveSubsystemBackRightRotationTalon->SetControlMode(CANTalon::kPercentVbus);
-	RobotMap::swerveSubsystemFrontLeftRotationTalon->SetControlMode(CANTalon::kPercentVbus);
-	RobotMap::swerveSubsystemFrontRightRotationTalon->SetControlMode(CANTalon::kPercentVbus);
-
-	RobotMap::swerveSubsystemBackLeftRotationTalon->Set(.15);
-	RobotMap::swerveSubsystemBackRightRotationTalon->Set(.15);
-	RobotMap::swerveSubsystemFrontLeftRotationTalon->Set(.15);
-	RobotMap::swerveSubsystemFrontRightRotationTalon->Set(.15);*/
+	}*/
+	RobotMap::swerveSubsystemBackLeftRotationTalon->Set(ControlMode::PercentOutput, .15);
+	//RobotMap::swerveSubsystemBackRightRotationTalon->Set(ControlMode::PercentOutput, .15);
+	//RobotMap::swerveSubsystemFrontLeftRotationTalon->Set(ControlMode::PercentOutput, .15);
+	//RobotMap::swerveSubsystemFrontRightRotationTalon->Set(ControlMode::PercentOutput, .15);
 
 	testsPassed = true;
 }
--- SwerveRobot/src/RobotMap.cpp
+++ SwerveRobot/src/RobotMap.cpp
@@ -11,7 +11,7 @@ std::shared_ptr<WPI_TalonSRX> RobotMap::swerveSubsystemBackLeftRotationTalon;
 std::shared_ptr<WPI_TalonSRX> RobotMap::swerveSubsystemBackRightRotationTalon;
 
 std::shared_ptr<AHRS> RobotMap::robotIMU;
-std::shared_ptr<frc::PowerDistributionPanel> RobotMap::pdp;
+//std::shared_ptr<frc::PowerDistributionPanel> RobotMap::pdp;
 std::shared_ptr<TigerDrive> RobotMap::tigerDrive;
 std::shared_ptr<TigerSwerve> RobotMap::tigerSwerve;
 
@@ -38,15 +38,19 @@ void RobotMap::init() {
 	swerveSubsystemFrontRightDriveTalon->SetInverted(true);
 
 	int encoderAbsolutePositionFrontLeft = swerveSubsystemFrontLeftRotationTalon->GetSensorCollection().GetPulseWidthPosition() & 0xFFF;
+	std::cout << "FLPS: " << encoderAbsolutePositionFrontLeft << std::endl;
 	swerveSubsystemFrontLeftRotationTalon->GetSensorCollection().SetPulseWidthPosition(encoderAbsolutePositionFrontLeft, 10);
 
 	int encoderAbsolutePositionFrontRight = swerveSubsystemFrontRightRotationTalon->GetSensorCollection().GetPulseWidthPosition() & 0xFFF;
+	std::cout << "FRPS: " << encoderAbsolutePositionFrontRight << std::endl;
 	swerveSubsystemFrontRightRotationTalon->GetSensorCollection().SetPulseWidthPosition(encoderAbsolutePositionFrontRight, 10);
 
 	int encoderAbsolutePositionBackLeft = swerveSubsystemBackLeftRotationTalon->GetSensorCollection().GetPulseWidthPosition() & 0xFFF;
+	std::cout << "BLPS: " << encoderAbsolutePositionBackLeft << std::endl;
 	swerveSubsystemBackLeftRotationTalon->GetSensorCollection().SetPulseWidthPosition(encoderAbsolutePositionBackLeft, 10);
 
 	int encoderAbsolutePositionBackRight = swerveSubsystemBackRightRotationTalon->GetSensorCollection().GetPulseWidthPosition() & 0xFFF;
+	std::cout << "BRPS: " << encoderAbsolutePositionBackRight << std::endl;
 	swerveSubsystemBackRightRotationTalon->GetSensorCollection().SetPulseWidthPosition(encoderAbsolutePositionBackRight, 10);
 
 	swerveSubsystemFrontLeftRotationTalon->ConfigSelectedFeedbackSensor(ctre::phoenix::motorcontrol::FeedbackDevice::CTRE_MagEncoder_Relative, 0, 10);
@@ -54,46 +58,46 @@ void RobotMap::init() {
 	swerveSubsystemBackLeftRotationTalon->ConfigSelectedFeedbackSensor(ctre::phoenix::motorcontrol::FeedbackDevice::CTRE_MagEncoder_Relative, 0, 10);
 	swerveSubsystemBackRightRotationTalon->ConfigSelectedFeedbackSensor(ctre::phoenix::motorcontrol::FeedbackDevice::CTRE_MagEncoder_Relative, 0, 10);
 
-	swerveSubsystemFrontLeftRotationTalon->SetSensorPhase(true);
-	swerveSubsystemFrontRightRotationTalon->SetSensorPhase(true);
+	swerveSubsystemFrontLeftRotationTalon->SetSensorPhase(false);
+	swerveSubsystemFrontRightRotationTalon->SetSensorPhase(false);
 	swerveSubsystemBackLeftRotationTalon->SetSensorPhase(true);
-	swerveSubsystemBackRightRotationTalon->SetSensorPhase(true);
+	swerveSubsystemBackRightRotationTalon->SetSensorPhase(false);
 
-	swerveSubsystemFrontLeftRotationTalon->SetInverted(false);
-	swerveSubsystemFrontRightRotationTalon->SetInverted(false);
+	swerveSubsystemFrontLeftRotationTalon->SetInverted(true);
+	swerveSubsystemFrontRightRotationTalon->SetInverted(true);
 	swerveSubsystemBackLeftRotationTalon->SetInverted(false);
-	swerveSubsystemBackRightRotationTalon->SetInverted(false);
+	swerveSubsystemBackRightRotationTalon->SetInverted(true);
 
 	swerveSubsystemFrontLeftRotationTalon->Set(ctre::phoenix::motorcontrol::ControlMode::Position, 0);
 	swerveSubsystemFrontRightRotationTalon->Set(ctre::phoenix::motorcontrol::ControlMode::Position, 0);
 	swerveSubsystemBackLeftRotationTalon->Set(ctre::phoenix::motorcontrol::ControlMode::Position, 0);
 	swerveSubsystemBackRightRotationTalon->Set(ctre::phoenix::motorcontrol::ControlMode::Position, 0);
 
-	swerveSubsystemFrontLeftRotationTalon->Config_kP(0, 6, 10);
+	swerveSubsystemFrontLeftRotationTalon->Config_kP(0, .2, 10);
 	swerveSubsystemFrontLeftRotationTalon->Config_kI(0, 0, 10);
-	swerveSubsystemFrontLeftRotationTalon->Config_kD(0, 15, 10);
+	swerveSubsystemFrontLeftRotationTalon->Config_kD(0, 0, 10);
 	swerveSubsystemFrontLeftRotationTalon->ConfigPeakOutputForward(1, 10);
 	swerveSubsystemFrontLeftRotationTalon->ConfigPeakOutputReverse(-1, 10);
 	swerveSubsystemFrontLeftRotationTalon->ConfigAllowableClosedloopError(0, 35, 10);
 
 
-	swerveSubsystemFrontRightRotationTalon->Config_kP(0, 6, 10);
+	swerveSubsystemFrontRightRotationTalon->Config_kP(0, .2, 10);
 	swerveSubsystemFrontRightRotationTalon->Config_kI(0, 0, 10);
-	swerveSubsystemFrontRightRotationTalon->Config_kD(0, 15, 10);
+	swerveSubsystemFrontRightRotationTalon->Config_kD(0, 0, 10);
 	swerveSubsystemFrontRightRotationTalon->ConfigPeakOutputForward(1, 10);
 	swerveSubsystemFrontRightRotationTalon->ConfigPeakOutputReverse(-1, 10);
 	swerveSubsystemFrontRightRotationTalon->ConfigAllowableClosedloopError(0, 35, 10);
 
-	swerveSubsystemBackLeftRotationTalon->Config_kP(0, 6, 10);
+	swerveSubsystemBackLeftRotationTalon->Config_kP(0, .2, 10);
 	swerveSubsystemBackLeftRotationTalon->Config_kI(0, 0, 10);
-	swerveSubsystemBackLeftRotationTalon->Config_kD(0, 15, 10);
+	swerveSubsystemBackLeftRotationTalon->Config_kD(0, 0, 10);
 	swerveSubsystemBackLeftRotationTalon->ConfigPeakOutputForward(1, 10);
 	swerveSubsystemBackLeftRotationTalon->ConfigPeakOutputReverse(-1, 10);
 	swerveSubsystemBackLeftRotationTalon->ConfigAllowableClosedloopError(0, 35, 10);
 
-	swerveSubsystemBackRightRotationTalon->Config_kP(0, 6, 10);
+	swerveSubsystemBackRightRotationTalon->Config_kP(0, .2, 10);
 	swerveSubsystemBackRightRotationTalon->Config_kI(0, 0, 10);
-	swerveSubsystemBackRightRotationTalon->Config_kD(0, 15, 10);
+	swerveSubsystemBackRightRotationTalon->Config_kD(0, 0, 10);
 	swerveSubsystemBackRightRotationTalon->ConfigPeakOutputForward(1, 10);
 	swerveSubsystemBackRightRotationTalon->ConfigPeakOutputReverse(-1, 10);
 	swerveSubsystemBackRightRotationTalon->ConfigAllowableClosedloopError(0, 35, 10);
@@ -113,6 +117,6 @@ void RobotMap::init() {
 
     robotIMU.reset(new AHRS(frc::SPI::Port::kMXP));
 
-    pdp.reset(new frc::PowerDistributionPanel());
+    //pdp.reset(new frc::PowerDistributionPanel());
     tigerDrive.reset(new TigerDrive(robotIMU.get()));
 }
